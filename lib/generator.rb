5b355406e5f204143ad5ff42fc0f480a4537b495
John Napier <jcee.napier@gmail.com>
2021-11-10 19:11:01 -0700

    Add Generator class with alphabet.

---
+++ lib/generator.rb
@@ -0,0 +1,7 @@
+class Generator
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+  end
+end 
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -7,3 +7,15 @@ describe Generator do
   it 'exists' do
     expect(generator).to be_an_instance_of(Generator)
   end
+
+  it 'attributes' do
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
+      "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+      "u", "v", "w", "x", "y", "z", " "]
+
+    expect(generator.alphabet).to eq(expected)
+    expect(generator.alphabet).to be_an(Array)
+    expect(generator.alphabet.length).to eq(27)
+    expect(generator.alphabet.sample.class).to eq(String)
+  end
+end 
