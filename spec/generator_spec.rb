3d8dc6d124d969c77d157d61c9601c7ad7bac759
John Napier <jcee.napier@gmail.com>
2021-11-10 19:27:08 -0700

    Add Generate key test

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -3,6 +3,7 @@ require "./lib/generator"
 
 describe Generator do
   let(:generator) {Generator.new}
+  let(:range) {Range.new(1, 9)}
 
   it 'exists' do
     expect(generator).to be_an_instance_of(Generator)
@@ -17,5 +18,13 @@ describe Generator do
     expect(generator.alphabet).to be_an(Array)
     expect(generator.alphabet.length).to eq(27)
     expect(generator.alphabet.sample.class).to eq(String)
+  end
+
+  it '#key generator' do
+    expect(generator.key_generator).to be_an(Array)
+    expect(generator.key_generator.first.class).to eq(Integer)
+    expect(generator.key_generator.last.class).to eq(Integer)
+    expect(generator.key_generator.sample.class).to eq(Integer)
+    expect(range.include?(generator.key_generator.length)).to be(true)
   end
 end 
