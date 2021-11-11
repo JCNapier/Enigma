758b8d643b8cf3d7ac12358be28cee58c6001ab6
John Napier <jcee.napier@gmail.com>
2021-11-10 19:44:06 -0700

    Add key_generator method

--- lib/generator.rb
+++ lib/generator.rb
@@ -4,4 +4,16 @@ class Generator
   def initialize
     @alphabet = ("a".."z").to_a << " "
   end
+
+     #generates a random key as integer
+  def key_generator
+    key = []
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    lengths = [1, 2, 3, 4, 5]
+
+    (lengths.sample).times do
+      key << numbers.sample
+    end
+    key
+  end
 end 
