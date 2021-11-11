b532ea72034af1517da31886dc2700520c4ad309
John Napier <jcee.napier@gmail.com>
2021-11-10 22:10:53 -0700

    Add Enigma attributes with attributes test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,8 +3,17 @@ require_relative "generator"
 require_relative "alphabet_index"
 
 class Enigma
-  def initialize
-    
+  attr_reader :generator,
+              :alphabet_hash,
+              :gen_key, 
+              :gen_time, 
+              :encrypted
+
+  def initialize 
+    @generator      = Generator.new 
+    @alphabet_hash  = AlphabetIndex.new
+    @gen_key        = @generator.key_padding(@generator.key_generator) 
+    @gen_time       = @generator.offset_generator
+    @encrypted      = {encryption: nil, key: nil, date: nil}
   end
- 
 end 
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,4 +11,15 @@ describe Enigma do
   it 'exists' do
     expect(enigma).to be_an_instance_of(Enigma)
   end
+
+  it 'attributes' do
+    expect(enigma.generator).to be_an_instance_of(Generator)
+    expect(enigma.alphabet_hash).to be_an_instance_of(AlphabetIndex)
+    expect(enigma.gen_time.length).to eq(4)
+    expect(enigma.gen_time).to be_a(String)
+    expect(enigma.gen_key).to be_a(String)
+    expect(enigma.gen_key.length).to eq(5)
+    expect(enigma.encrypted).to be_a(Hash)
+    expect(enigma.encrypted.length).to eq(3)
+  end
 end 
